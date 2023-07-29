acdc3b7b323e17abcb52960bacbeb2e5933f1b3b
Hiromasa Ihara <iharahiromasa@gmail.com>
2023-07-30 02:19:07 +0900

    [from now] 2023/07/30 02:19:07
     jpphonechecker/__main__.py | 5 ++++-
     1 file changed, 4 insertions(+), 1 deletion(-)

--- jpphonechecker/__main__.py
+++ jpphonechecker/__main__.py
@@ -1,12 +1,15 @@
+"""__main__."""
+from pprint import pprint as pp
+
 import click
 
 from . import checker
-from pprint import pprint as pp
 
 
 @click.command
 @click.argument('phone-number')
 def main(phone_number):
+    """電話番号を検索する."""
     r = checker.telenavi(phone_number)
     pp(r)
     r = checker.jpnumber(phone_number, headless=True)
